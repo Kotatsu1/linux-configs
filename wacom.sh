896fef306688668bc42fc766c37b22768fc01730
Kotatsu1 <nikitakotatsu@gmail.com>
2024-05-18 18:16:42 +0300

    auto install script

--- wacom.sh
+++
@@ -1,3 +0,0 @@
-xsetwacom set "Wacom Intuos PT S Pen stylus" Area 4350 2447 10850 6103
-xsetwacom set "Wacom Intuos PT S Pen stylus" MapToOutput 1920x1080+0+0
-xsetwacom set "Wacom Intuos PT S Pen stylus" RawSample 1
